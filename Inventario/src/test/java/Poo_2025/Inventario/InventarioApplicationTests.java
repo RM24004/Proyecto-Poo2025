54210fb97eacd51d404a44395e8b912e1beb21ac
victor64x4 <rm24004@ues.edu.sv>
2025-09-19 13:11:23 -0600

    nuevo projecto

---
+++ Inventario/src/test/java/Poo_2025/Inventario/InventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package Poo_2025.Inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
